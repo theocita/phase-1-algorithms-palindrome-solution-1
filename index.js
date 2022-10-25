df9ed4254726c1df94e5265f5238947dc7df8d7e
theocita <theocitaallen@gmail.com>
2022-10-25 18:07:15 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+
+  return reversedWord;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  take a word and iterate over each letter,
+  reverse the word,
+  join the letters together again
+
+  return the reverse word
+
+  reversed word should strictly equal to word
 */
 
 /*
   Add written explanation of your solution here
+  take the word and seperate it but each letter.  
+  Reverse the letters,
+  Join the letters 
+ the reverse word should be a palindrome when reversed. if not it is false
 */
 
 // You can run `node index.js` to view these console logs
